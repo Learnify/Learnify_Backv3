1ad566f86db78c3197df8a157d69b2ed0d566611
Julian Moreno <bjmorenon@unal.eu.co>
2019-02-07 23:52:27 -0500

    Relationship between User and Role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_08_041558) do
+ActiveRecord::Schema.define(version: 2019_02_08_044951) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2019_02_08_041558) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role_id"
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
 end
