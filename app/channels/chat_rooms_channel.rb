bbc2615d9ea495db8a6e79f2ea69c4e730361d44
Julian Moreno <bjmorenon@unal.eu.co>
2019-03-09 17:20:45 -0500

    mailers OK

--- app/channels/chat_rooms_channel.rb
+++ app/channels/chat_rooms_channel.rb
@@ -1,13 +1,40 @@
 class ChatRoomsChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "chat_rooms_#{params['chat_room_id']}_channel"
+    if params[:room_id].present?
+      # creates a private chat room with a unique name
+      stream_from("ChatRoom-#{(params[:room_id])}")
+    end
   end
+  
+  # calls when a client broadcasts data
+  def speak(data)
+    sender    = get_sender(data)
+    room_id   = data['room_id']
+    message   = data['message']
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    raise 'No room_id!' if room_id.blank?
+    convo = get_convo(room_id) # A conversation is a room
+    raise 'No conversation found!' if convo.blank?
+    raise 'No message!' if message.blank?
+
+    # adds the message sender to the conversation if not already included
+    convo.users << sender unless convo.users.include?(sender)
+    # saves the message and its data to the DB
+    # Note: this does not broadcast to the clients yet!
+    Message.create!(
+      conversation: convo,
+      sender: sender,
+      content: message
+    )
   end
-
-  def send_message(data)
-    current_user.messages.create!(body: data['message'], chat_room_id: data['chat_room_id'])
+  
+  # Helpers
+  
+  def get_convo(room_code)
+    Conversation.find_by(room_code: room_code)
+  end
+  
+  def set_sender
+    User.find_by(guid: id)
   end
 end
